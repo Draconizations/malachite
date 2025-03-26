504689e72edcba40ae69c79db20996822d224e38
Jake <jake@fulmine.xyz>
2025-03-26 09:50:54 +0100

    don't shallow copy again

--- src/malachite.ts
+++ src/malachite.ts
@@ -13,11 +13,11 @@ import { emptyData } from "./state.ts";
 
 	Alpine.start()
 	window.Alpine.store("story", emptyData)
-	window.$s = { ...Alpine.store("story") as any } // create a shallow copy, so that it is available during user scripts
+	window.$s = Alpine.store("story") as any // so that it is available during user scripts
 	
 	Engine.runUserScripts()
 	State.init()
-	window.$s = { ...Alpine.store("story") as any } // clone the object again because we reassigned it in State.init()
+	window.$s = Alpine.store("story") as any // clone the object again because we reassigned it in State.init()
 
 	// TODO: load any potential startup passages here?
 
