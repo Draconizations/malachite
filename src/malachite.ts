a60467a642f2a9b73400cc08065db0269b76bea7
Jake <jake@fulmine.xyz>
2025-03-26 09:04:31 +0100

    init story store before user scripts are loaded

--- src/malachite.ts
+++ src/malachite.ts
@@ -3,18 +3,19 @@ import Story from "./story.ts"
 import State from "./state.ts"
 import Alpine from "./alpine.ts"
 import setupGlobals from "./api.ts"
+import { emptyData } from "./state.ts";
 
-;(async () => {
+(async () => {
 	setupGlobals()
 
 	Engine.init()
 	Story.init()
 
 	Alpine.start()
+	window.Alpine.store("story", emptyData)
 	window.$s = Alpine.store("story") as any
 	
 	Engine.runUserScripts()
-
 	State.init()
 
 	// TODO: load any potential startup passages here?
