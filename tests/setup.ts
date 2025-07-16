c4630eedf92eabeb70dd5106c1b2f0e9fed85210
Jake <jake@fulmine.xyz>
2025-07-16 07:20:54 +0200

    fix tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,12 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator"
+import pkg from "../package.json" with { type: "json " }
 
 GlobalRegistrator.register()
+
+declare global {
+	interface Window {
+		__VERSION__: string
+	}
+}
+
+window.__VERSION__ = pkg.version
